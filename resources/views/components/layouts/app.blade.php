bf673104cc865670ee2ef8bcab150ce4f56964ed
mari@beesgo.co <mgaribov@estee.com>
2024-03-06 14:12:26 -0500

    feat: use livewire as homepage, livewire layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Hello!' }}</title>
+
+        @vite('resources/css/app.css')
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    </head>
+    <body class="p-4">
+      {{ $slot }}
+    </body>
+</html>
