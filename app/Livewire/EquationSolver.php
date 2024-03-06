693770708e5ad4a363786c63e74d5ac05b731abe
mari@beesgo.co <mgaribov@estee.com>
2024-03-06 15:13:29 -0500

    feat: validateEquation, equationIsEmpty, charactersAreValid, variablesCorrectlyPositioned

--- app/Livewire/EquationSolver.php
+++ app/Livewire/EquationSolver.php
@@ -15,8 +15,11 @@ class EquationSolver extends Component
 
     public string $newEquationEntry;
 
+    public bool $buttonsDisabled = false;
+
     public string $equationSyntaxError;
     public string $equationSubmittedError;
+
 
     public function messages()
     {
@@ -42,15 +45,66 @@ class EquationSolver extends Component
         $newEquationArray = preg_split("/,/", $this->newEquationEntry);
 
         foreach($newEquationArray as $newEquation) {
-            // validate equation syntax
-            dump($newEquation);
+            $validatedEquation = $this->validateEquation($newEquation);
+            if($validatedEquation == null || $validatedEquation == '') break;
+
+            dd($validatedEquation);
+
         }
 
         $this->reset('newEquationEntry');
     }
 
+    private function validateEquation(string $equation): ?string
+    {
+        if ($this->equationIsEmpty($equation)) return null;
 
+        $trimmedEquation = strtolower(preg_replace('/\s+/', '', $equation));
 
+        if (!$this->charactersAreValid($trimmedEquation)) return null;
+        if (!$this->variablesCorrectlyPositioned($trimmedEquation)) return null;
+
+        return $trimmedEquation;
+    }
+
+    private function equationIsEmpty(string $equation): bool
+    {
+        if($equation == '' || $equation == null || $equation == ' ') {
+            $this->equationSyntaxError = 'Entry is empty or you had two commas next to each other!';
+            $this->equationSubmittedError = 'You submitted: ' . $equation;
+            return true;
+        }
+        return false;
+    }
+
+    private function charactersAreValid(string $equation): bool
+    {
+        if( trim($equation, 'a..z0..9+-') !== '' ) {
+            $this->equationSyntaxError = 'Invalid characters, we only support addition and subtraction.';
+            $this->equationSubmittedError = 'You submitted: ' . $equation;
+            return false;
+        }
+
+        $operators = array('+', '-');
+        if( in_array(substr($equation, -1), $operators) ) {
+            $this->equationSyntaxError = 'Last character must be a number or variable.';
+            $this->equationSubmittedError = 'You submitted: ' . $equation;
+            return false;
+        }
+
+        return true;
+    }
+
+    private function variablesCorrectlyPositioned(string $equation): bool
+    {
+        if( preg_match('/\d[a-z]|[a-z]\.|\.[a-z]|[a-z]\d|[a-z][a-z]|[+-][+-]/',  $equation) ) {
+            $this->equationSyntaxError = 'Letter variable cannot be directly preceded or followed by a number. Two letter variables or two operators also cannot be directly next to each other';
+            $this->equationSubmittedError = 'You submitted: ' . $equation;
+            return false;
+        }
+
+        return true;
+    }
 
     #[Title('Simple Equation Solver')]
     public function render()
--- resources/views/livewire/equation-solver.blade.php
+++ resources/views/livewire/equation-solver.blade.php
@@ -1,38 +1,64 @@
 <div class="p-4 flex flex-col gap-10">
 
-    <h1 class="uppercase text-bold text-secondary text-4xl">Simple Equation Summer</h1>
-
-    <form wire:submit="addNewEquation" class="flex flex-col md:flex-row gap-4">
-        <div class="flex-1 relative">
-            <label for="equationInput" class="text-sm bg-white absolute -top-3 left-2 px-2">Equations:</label>
-            <input
-                    class="p-4 border border-stone-300 w-full"
-                    type="text"
-                    id="equationInput"
-                    placeholder="1+1"
-                    wire:model="newEquationEntry">
-        </div>
-        <button
-                class="px-4 py-2 border border-stone-300 rounded-md hover:button-text-hover hover:text-button-hover transition-all disabled:text-button-disabled disabled:bg-button-disabled disabled:cursor-not-allowed max-w-fit font-bold"
-                type="submit"
-        >
-            Add Equation
-        </button>
-    </form>
-
-    <div class="mt-4">
-        @error('newEquationEntry')
-        <div class="bg-error border border-error text-error rounded-md p-4">
-            {{ $message }}
-        </div>
-        @enderror
-        @if ($equationSyntaxError)
-            <div class="bg-error border border-error text-error rounded-md p-4 flex flex-col gap-3 text-sm">
-                <p>{{ $equationSyntaxError }}</p>
-            </div>
-        @endif
+    <div>
+        <h1 class="uppercase text-bold text-secondary text-4xl">Simple Equation Summer</h1>
+        <p class="pt-4">
+            Use this tool to calculate up to 26 addition or subtraction equations.
+            <br/>You can use previous equations as variables in equations further down the line.
+        </p>
     </div>
 
-    {{ $newEquationEntry }}
+    <div>
+        <form wire:submit="addNewEquation" class="flex flex-col md:flex-row gap-4">
+            <div class="flex-1 relative">
+                <label for="equationInput" class="text-sm bg-white absolute -top-3 left-2 px-2">Equations:</label>
+                <input
+                        class="p-4 border border-stone-300 w-full"
+                        type="text"
+                        id="equationInput"
+                        placeholder="1+1"
+                        wire:model="newEquationEntry">
+            </div>
+            <button
+                    class="px-4 py-2 border border-stone-300 rounded-md hover:button-text-hover hover:text-button-hover transition-all disabled:text-button-disabled disabled:bg-button-disabled disabled:cursor-not-allowed max-w-fit font-bold"
+                    type="submit"
+                    @if($buttonsDisabled) disabled @endif
+            >
+                Add Equation
+            </button>
+        </form>
+
+        <div class="mt-4">
+            @error('newEquationEntry')
+                <div class="bg-error border border-error text-error rounded-md p-4">
+                    {{ $message }}
+                </div>
+            @enderror
+            @if ($equationSyntaxError)
+                <div class="bg-error border border-error text-error rounded-md p-4 flex flex-col gap-3 text-sm">
+                    <p>{{ $equationSyntaxError }}</p>
+                    <p>If you submitted multiple equations, valid ones listed before the error were added.</p>
+                    <p class="font-bold">{{ $equationSubmittedError }}</p>
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="flex flex-col gap-4 bg-neutral-100 p-6">
+        <div class="flex flex-col md:flex-row gap-4">
+            <div class="p-6 flex-1">
+                <h2 class="font-bold text-xl mb-4">Equations To Sum</h2>
+                @if($equations)
+                    @foreach ($equations as $key => $equation)
+                        <div>
+                            <span class="font-bold">{{ $key }}:</span> {{ $equation }}
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+    </div>
+
+
 
 </div>
